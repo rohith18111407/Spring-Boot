84d15d680dea332cdda0fa5196ff78cd23076348
Rohith M <108167442+rohith18111407@users.noreply.github.com>
2024-07-04 10:11:38 +0530

    Add files via upload

---
+++ MyWebApp-1/src/main/java/com/telusko/demo/MyController.java
@@ -0,0 +1,22 @@
+package com.telusko.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	  @GetMapping("home")
+	  
+	    @ResponseBody
+	    public String home() 
+	    {
+	        System.out.println("hi");
+	        return "home";
+	    }
+}
+
+
+
+
+
